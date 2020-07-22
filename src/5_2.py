360f4e4b691bff6fbf42a7920025ab896f32b1d6
Ivan <ivanshahter71@gmail.com>
2020-07-22 14:44:39 +0300

    task_ 5_2.py

--- src/5_2.py
+++ src/5_2.py
@@ -7,10 +7,11 @@ summa, mult, tmp = 0, 1, 1
 
 try:
     number = int(number)
-    for i in range(length):
-        summa += number // (1 * tmp) % 10
-        mult *= number // (1 * tmp) % 10
+    while length > 0:
+        summa += number // tmp % 10
+        mult *= number // tmp % 10
         tmp *= 10
+        length -= 1
     print(f"Summa: {summa}")
     print(f"Mult: {mult}")
 
