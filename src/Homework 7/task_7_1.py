22f3f32902111624a5be328ba7fb5bddcb76bff9
Ivan <ivanshahter71@gmail.com>
2020-07-22 19:15:25 +0300

    task_7.py

---
+++ src/Homework 7/task_7_1.py
@@ -0,0 +1,50 @@
+# task_7_1
+"""Написать 12 функций по переводу"""
+
+
+def inch_cm(value: float) -> float:
+    return value * 2.54
+
+
+def cm_inch(value: float) -> float:
+    return value / 2.54
+
+
+def mile_km(value: float) -> float:
+    return value * 1.609
+
+
+def km_mile(value: float) -> float:
+    return value / 1.609
+
+
+def pounds_kg(value: float) -> float:
+    return value * 2.205
+
+
+def kg_pounds(value: float) -> float:
+    return value / 2.205
+
+
+def ounce_g(value: float) -> float:
+    return value * 28.35
+
+
+def g_ounce(value: float) -> float:
+    return value / 28.35
+
+
+def gallon_l(value: float) -> float:
+    return value * 3.785
+
+
+def l_gallon(value: float) -> float:
+    return value / 3.785
+
+
+def pints_l(value: float) -> float:
+    return value * 2.113
+
+
+def l_pints(value: float) -> float:
+    return value / 2.113
