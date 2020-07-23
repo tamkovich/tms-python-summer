a6a40533cfe564a82b54ac09fd69dd4358bbe7a8
Lex-23 <Liverpoolsmile23@gmail.com>
2020-07-23 15:27:21 +0300

    HW5:Alexey Valento

--- src/task_5_6.py
+++ src/task_5_6.py
@@ -12,6 +12,6 @@ count = 0  # счетчик участков роста
 for i, el in enumerate(list_1):
     if i == (len(list_1) - 1):
         break
-    if el < list_1[i+1] and el < list_1[i-2]:
+    if el < list_1[i+1] and el < list_1[i-1]:
         count += 1
 print(f'count area of rice: {count}')  # количество участков роста
