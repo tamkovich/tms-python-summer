8bdc99c9b6532a65cfc589373d8c89a15abdf8d5
Lex-23 <Liverpoolsmile23@gmail.com>
2020-07-23 15:21:54 +0300

    HW5: Alexey Valento

--- src/task_5_8.py
+++ src/task_5_8.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+"""
+В заданной строке расположить в обратном порядке все слова. Разделителями
+слов считаются пробелы.
+"""
 strok_1 = input("some stroke: ")
-=======
-strok_1 = str(input("some stroke: "))
->>>>>>> 2384a922be89f763ddae83595a1200eee8a3f202
-
 strok_revers = ' '.join(strok_1.split(' ')[::-1])
 print(f'output: {strok_revers}')
--- src/task_5_9.py
+++ src/task_5_9.py
@@ -1,13 +1,28 @@
+"""
+Для каждого натурального числа в промежутке от m до n вывести все делители,
+кроме единицы и самого числа. m и n вводятся с клавиатуры.
+Пример:m =100, n = 105
+100: 2 4 5 10 20 25 50
+101:
+102: 2 3 6 17 34 51
+103:
+104: 2 4 8 13 26 52
+105: 3 5 7 15 21 35
+"""
+
 """Ввести два натуральных числа, отличных от единицы и нуля"""
-m = abs(int(input('Enter some number:')))
-n = abs(int(input('Enter some number')))
+try:
+    m = abs(int(input('Enter some number:')))
+    n = abs(int(input('Enter some number')))
+except ValueError:
+    print("m and m must be intenger")
 m = min(m, n)
 
 for num in range(m, n + 1):
     divisions = []  # доступные делители будут собираться в этот список
     dict_divisions = {}
 
-    for div in range(2, num - 1):
+    for div in range(2, int(num/2)):
         if num % div == 0:
             divisions.append(div)
             dict_divisions.update({num: divisions})
