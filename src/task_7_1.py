d9a61a75688befcce8f08c7008688acb1db810e7
Farruh Sherpov <sheripov.farruh@gmail.com>
2020-07-27 20:56:04 +0300

    Complate

--- src/task_7_1.py
+++ src/task_7_1.py
@@ -1,96 +1,136 @@
-# 1. Дюймы в сантиметры
-def inch_to_sm(a):
+def inch_to_sm(a: int) -> float:
+    """
+    1. Дюймы в сантиметры
+    :param a: Дюймы
+    :return: сантиметры
+    """
     return a / 0.39
 
 
-# 2. Сантиметры в дюймы
-def sm_to_inch(a):
+def sm_to_inch(a: int) -> float:
+    """
+    2. Сантиметры в дюймы
+    :param a: Сантиметры
+    :return: дюймы
+    """
     return a * 0.39
 
 
-# 3. Мили в километры
-def mile_to_km(a):
+def mile_to_km(a: int) -> float:
+    """
+    3. Мили в километры
+    :param a: Мили
+    :return: километры
+    """
     return a / 0.609344
 
 
-# 4. Километры в мили
-def km_to_mile(a):
+def km_to_mile(a: int) -> float:
+    """
+    4. Километры в мили
+    :param a: Километры
+    :return: мили
+    """
     return a * 0.609344
 
 
-# 5. Фунты в килограммы
-def pounds_to_kg(a):
+def pounds_to_kg(a: int) -> float:
+    """
+    5. Фунты в килограммы
+    :param a: Фунты
+    :return: килограммы
+    """
     return a * 0.454
 
 
-# 6. Килограммы в фунты
-def kg_to_pounds(a):
+def kg_to_pounds(a: int) -> float:
+    """
+    6. Килограммы в фунты
+    :param a: Килограммы
+    :return: фунты
+    """
     return a / 0.454
 
 
-# 7. Унции в граммы
-def ounce_to_gr(a):
+def ounce_to_gr(a: int) -> float:
+    """
+    7. Унции в граммы
+    :param a: Унции
+    :return: граммы
+    """
     return a * 28.34952312
 
 
-# 8. Граммы в унции
-def gr_to_ounce(a):
+def gr_to_ounce(a: int) -> float:
+    """
+    8. Граммы в унции
+    :param a: Граммы
+    :return: унции
+    """
     return a / 28.34952312
 
 
-# 9. Галлон в литры
-def gallons_to_liters(a):
+def gallons_to_liters(a: int) -> float:
+    """
+    9. Галлон в литры
+    :param a: Галлон
+    :return: литры
+    """
     return a / 4.54609926499
 
 
-# 10. Литры в галлоны
-def liters_to_gallons(a):
+def liters_to_gallons(a: int) -> float:
+    """
+    10. Литры в галлоны
+    :param a: Литры
+    :return: галлоны
+    """
     return a * 4.54609926499
 
 
-# 11. Пинты в литры
-def pints_to_liters(a):
+def pints_to_liters(a: int) -> float:
+    """
+    11. Пинты в литры
+    :param a:Пинты
+    :return:литры
+    """
     return a * 0.56826125
 
 
-# 12. Литры в пинты
-def liters_to_pints(a):
+def liters_to_pints(a: int) -> float:
+    """
+    12. Литры в пинты
+    :param a: Литры
+    :return: пинты
+    """
     return a / 0.56826125
 
 
 while True:
-    choose = input('\nChoose operation: ')
+
     try:
-        if choose == '0':
-            break
-        elif choose == '':
-            print('you must enter number in range 1-12 or 0 to exit!')
-            continue
-        elif choose == '1':
-            print(inch_to_sm(float(input('input number: '))))
-        elif choose == '2':
-            print(sm_to_inch(float(input('input number: '))))
-        elif choose == '3':
-            print(mile_to_km(float(input('input number: '))))
-        elif choose == '4':
-            print(km_to_mile(float(input('input number: '))))
-        elif choose == '5':
-            print(pounds_to_kg(float(input('input number: '))))
-        elif choose == '6':
-            print(kg_to_pounds(float(input('input number: '))))
-        elif choose == '7':
-            print(ounce_to_gr(float(input('input number: '))))
-        elif choose == '8':
-            print(gr_to_ounce(float(input('input number: '))))
-        elif choose == '9':
-            print(gallons_to_liters(float(input('input number: '))))
-        elif choose == '10':
-            print(liters_to_gallons(float(input('input number: '))))
-        elif choose == '11':
-            print(pints_to_liters(float(input('input number: '))))
-        elif choose == '12':
-            print(liters_to_pints(float(input('input number: '))))
-        else:
+        choose = input('\nChoose operation: ')
+        if int(choose) not in range(1, 13):
             print('input in range 1-12')
+            continue
+        number = float(input('input number: '))
     except ValueError:
         print('error input!')
+        continue
+    switch_case = {
+        "1": inch_to_sm,
+        "2": sm_to_inch,
+        "3": mile_to_km,
+        "4": km_to_mile,
+        "5": pounds_to_kg,
+        "6": kg_to_pounds,
+        "7": ounce_to_gr,
+        "8": gr_to_ounce,
+        "9": gallons_to_liters,
+        "10": liters_to_gallons,
+        "11": pints_to_liters,
+        "12": liters_to_pints,
+    }
+    result = switch_case.get(choose)(number)
+    print(result)
+
