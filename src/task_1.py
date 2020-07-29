b29ad2600587a2e2e78dcdc4991d017355ab242f
Sheripov Farruh <sheripov.farruh@gmail.com>
2020-07-30 00:04:37 +0300

    complete hw-9

--- src/task_1.py
+++ src/task_1.py
@@ -1,4 +1,3 @@
 random_list = [i for i in range(67, 74)]
-formated_list = lambda string: [f'{index} - {value}' for index, value in enumerate(string)]
 print(random_list)
-print(formated_list(random_list))
+print((lambda string: [f'{index} - {value}' for index, value in enumerate(string)])(random_list))
--- src/task_2.py
+++ src/task_2.py
@@ -1,4 +1,3 @@
 random_list = {'sds': 3, 'dsadf': 4, 'e4rwes': 5}
-formated_list = lambda string: {key + key: string[key] for key in string}
 print(random_list)
-print(formated_list(random_list))
+print((lambda string: {key + key: string[key] for key in string})(random_list))
