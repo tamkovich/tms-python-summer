e62a1fb669df0167d6ccf8e43321916093d716ef
Sheripov Farruh <sheripov.farruh@gmail.com>
2020-07-30 00:07:38 +0300

    complete hw-9

--- src/task_1.py
+++ src/task_1.py
@@ -1,4 +1,5 @@
 random_list = [i for i in range(67, 74)]
 print(random_list)
-print((lambda string: [f'{index} - {value}' for index, value in enumerate(string)])
+print((lambda string:
+       [f'{index} - {value}' for index, value in enumerate(string)])
       (random_list))
