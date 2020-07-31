bac5231c07fe2aed3f9fc7e3116eabdcb62b1989
Lex-23 <Liverpoolsmile23@gmail.com>
2020-07-31 22:32:50 +0300

    HW9: Alexey Valento

---
+++ src/task_9/task_9_4.py
@@ -0,0 +1,44 @@
+"""
+Создать универсальный декоратор, который меняет порядок аргументов в
+функции на противоположный.
+"""
+
+def decor_arg_revers(func):
+    """
+    декоратор функции.
+    меняет порядок аргументов на проивоположный
+    :param func: функция с любым количеством аргументов
+    :return: функция с обратным порядком аргументов
+    """
+    def revers(*args):
+        revers_args = args[::-1]
+        try:
+            return(func(*revers_args))
+        except TypeError:
+            return "Error arguments type"
+    return revers
+
+@decor_arg_revers
+def f_sqw(a: int, b: int, c: int) -> str:
+    """
+    функция для решения квадратных уравнений
+    :param a: int
+    :param b: int
+    :param c: int
+    :return result: str(результат одной строкой)
+    """
+
+    D = b ** 2 - 4 * a * c
+    if D > 0:
+        x_1 = (-b + D ** .5)/(2*a)
+        x_2 = (-b - D ** .5)/(2*a)
+        result = f'x1 = {round(x_1, 2)}, x2 = {round(x_2, 2)}'
+    elif D == 0:
+        x = -b/(2*a)
+        result = f'x = {round(x, 2)}'
+    else:
+        result = 'нет корней'
+    return result
+
+print(f_sqw(10, 20, 6))
+print(f_sqw(6, 20, 10))  # для проверки без декоратора
