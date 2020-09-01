a0ce5b8b825e50597b7705f0bec30308ac094887
Lex-23 <Liverpoolsmile23@gmail.com>
2020-09-01 20:01:10 +0300

    HW13: Alexey Valento

---
+++ src/task_13/main.py
@@ -0,0 +1,22 @@
+"""
+Создать класс Matrix. Атрибуты - data(содержит саму матрицу - список
+списков), n, m. Определить конструктор(с параметрами(передача размерности:
+n, m и диапазона случайных чисел: a, b), по-умолчанию (матрица 5 на 5 где все
+элементы равны нулю), копирования) ,переопределить магический метод str для
+красивого вывода. Описать функции, которые принимают на вход объект класса
+Matrix. Функции позволяют искать максимальный элемент матрицы, минимальный,
+сумму всех элементов. Создать в файле main.py матрицу. Воспользоваться всеми
+описанными функциями и методами
+"""
+
+from matrix_utils import matrix_classes
+from matrix_utils import matrix_funcs
+
+if __name__ == "__main__":
+    mtrx = matrix_classes.Matrix(10, 50, 6, 6)
+    print(mtrx)
+    print(matrix_funcs.find_max_matrix_element(mtrx))
+    print(matrix_funcs.find_min_matrix_element(mtrx))
+    print(matrix_funcs.find_sum_matrix_elements(mtrx))
+    mtrx.gen_default_matrix()
+    print(mtrx)
---
+++ src/task_13/matrix_utils/matrix_funcs.py
@@ -0,0 +1,27 @@
+from matrix_utils.matrix_classes import Matrix
+import numpy as np
+
+
+def find_max_matrix_element(matrix: Matrix) -> int:
+    """
+    find max elem in matrix
+    :param matrix: matrix
+    :return: int
+    """
+    return (max(map(max, matrix.data)))
+
+def find_min_matrix_element(matrix: Matrix) -> int:
+    """
+    find min elem in matrix
+    :param matrix: matrix
+    :return: int
+    """
+    return (min(map(min, matrix.data)))
+
+def find_sum_matrix_elements(matrix: Matrix) -> int:
+    """
+    find sum elements in matrix
+    :param matrix: matrix
+    :return: int
+    """
+    return np.sum(matrix.data)
