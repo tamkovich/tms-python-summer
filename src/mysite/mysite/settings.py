796a1cc398a70f871ef7b04be0765cbe461fed2e
Denis Tamkovich <tamkovichdenis@gmail.com>
2020-09-21 20:26:45 +0300

    Added static

--- src/mysite/mysite/settings.py
+++ src/mysite/mysite/settings.py
@@ -96,8 +96,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
