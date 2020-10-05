91e49502b656370f95ac5f63b41a61d5a15a5577
Vitaliy Barahovskiy <vitalikbarahovskiy@mail.ru>
2020-10-05 18:46:10 +0300

    delete taskmanager

--- src/taskmanager/flymen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FlymenConfig(AppConfig):
-    name = 'flymen'
--- src/taskmanager/flymen/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name ='home'),
-    path('about-us', views.about, name ='about'),
-    path('creat', views.create, name ='create'),
-]
